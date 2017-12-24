ArtelMike/openwrt-1

commit 2c757baae1005ec02eb8b295ff4ff8bd3478c77e

ar71xx: spi-rb4xx fix.
In new kernels we should use clk_prepare_enable instead of clk_enable
since clk_enable does not make proper initialization that leads
to rise WARN_ON messages and not working spi bus on the device.

Signed-off-by: Sergey Sergeev <adron@yapic.net>

target/linux/ar71xx/files/drivers/spi/spi-rb4xx.c
@@ -413,7 +413,7 @@ static int rb4xx_spi_probe(struct platform_device *pdev)
 		goto err_put_master;
 	}
 
-	err = clk_enable(rbspi->ahb_clk);
+	err = clk_prepare_enable(rbspi->ahb_clk);
 	if (err)
 		goto err_clk_put;
 

commit 4163a922ec4b6e1da59a5ca3c324101bef8f241f

ar71xx: spi-rb4xx: balance clk_prepare_enable() call
Commit 2c757baae100 ("ar71xx: spi-rb4xx fix.") replaced the clk_enable() call
with clk_prepare_enable() to meet the common clock framework requirements.
However it did not change the clk_disable() call in the error patch which
thus leads to imbalance.

Fix the code by using the correct counterpart of clk_prepare_enable() in both
places.

Fixes: 2c757baae100 ("ar71xx: spi-rb4xx fix.")
Signed-off-by: Gabor Juhos <juhosg@freemail.hu>

target/linux/ar71xx/files/drivers/spi/spi-rb4xx.c
@@ -379,7 +379,7 @@ static int rb4xx_spi_probe(struct platform_device *pdev)
 err_iounmap:
 	iounmap(rbspi->base);
 err_clk_disable:
-	clk_disable(rbspi->ahb_clk);
+	clk_disable_unprepare(rbspi->ahb_clk);
 err_clk_put:
 	clk_put(rbspi->ahb_clk);
 err_put_master:
@@ -394,7 +394,7 @@ static int rb4xx_spi_remove(struct platform_device *pdev)
 	struct rb4xx_spi *rbspi = platform_get_drvdata(pdev);
 
 	iounmap(rbspi->base);
-	clk_disable(rbspi->ahb_clk);
+	clk_disable_unprepare(rbspi->ahb_clk);
 	clk_put(rbspi->ahb_clk);
 	platform_set_drvdata(pdev, NULL);
 	spi_master_put(rbspi->master);