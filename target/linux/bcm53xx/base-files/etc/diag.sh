ArtelMike/openwrt-1

commit a4a45a5e2c340509bcab481fd7f21dacb28d5432

bcm53xx: failsafe support
Signed-off-by: Rafał Miłecki <zajec5@gmail.com>

SVN-Revision: 43639

target/linux/bcm53xx/base-files/etc/diag.sh
@@ -1,0 +1,40 @@
+#!/bin/sh
+# Copyright (C) 2014 OpenWrt.org
+
+. /lib/functions/leds.sh
+
+get_status_led() {
+	local status_led_file
+
+	# There may be more than one color of power LED, try to avoid amber/red
+	status_led_file=$(find /sys/class/leds/ -name "*:power" -a ! -name "*:amber:*" -a ! -name "*:red:*" | head -n1)
+	if [ -d "$status_led_file" ]; then
+		status_led=$(basename $status_led_file)
+		return
+	fi;
+
+	# Now just pick any power LED
+	status_led_file=$(find /sys/class/leds/ -name "*:power:*" | head -n1)
+	if [ -d "$status_led_file" ]; then
+		status_led=$(basename $status_led_file)
+		return
+	fi;
+}
+
+set_state() {
+	get_status_led
+
+	[ -z "$status_led" ] && return
+
+	case "$1" in
+	preinit)
+		status_led_blink_preinit
+		;;
+	failsafe)
+		status_led_blink_failsafe
+		;;
+	done)
+		status_led_on
+		;;
+	esac
+}

target/linux/bcm53xx/base-files/lib/preinit/05_set_preinit_iface_bcm53xx
@@ -1,0 +1,7 @@
+#!/bin/sh
+
+set_preinit_iface() {
+	ifname=eth0
+}
+
+boot_hook_add preinit_main set_preinit_iface

commit 5c32c1214b68161d1b78c8e60db5aed6decef5d3

bcm53xx: fix fallback code for picking status LED
Looking for a wrong LED file name was stopping this code from find any
LED. This affects devices with only a red/amber power LED.

Fixes: a4a45a5e2c340 ("bcm53xx: failsafe support")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

target/linux/bcm53xx/base-files/etc/diag.sh
@@ -13,7 +13,7 @@ get_status_led() {
 	fi;
 
 	# Now just pick any power LED
-	status_led_file=$(find /sys/class/leds/ -name "*:power:*" | head -n1)
+	status_led_file=$(find /sys/class/leds/ -name "*:power" | head -n1)
 	if [ -d "$status_led_file" ]; then
 		status_led=$(basename $status_led_file)
 		return