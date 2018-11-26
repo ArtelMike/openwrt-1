ArtelMike/openwrt-1

commit 117b0f6cfbfa62a51a31880d661820a799d2ab2c

ar71xx: keep the RouterBOARD Power LED in On state
It is quite unexpected behaviour when the Power LED switches off as soon
as the kernel starts booting. So set the default state to 'Keep' for
the Power LEDs of all RouterBOARDs (e.g. RB91x, SXT Lite series, etc.).

Signed-off-by: Sergey Ryazanov <ryazanov.s.a@gmail.com>
[switch the default state to keep instead of on]
Signed-off-by: Mathias Kresin <dev@kresin.me>

target/linux/ar71xx/files/arch/mips/ath79/mach-rb91x.c
@@ -214,6 +214,7 @@ static struct gpio_led rb711gr100_leds[] __initdata = {
 		.name		= "rb:green:power",
 		.gpio		= RB91X_GPIO_LED_POWER,
 		.active_low	= 0,
+		.default_state	= LEDS_GPIO_DEFSTATE_KEEP,
 	},
 };
 

target/linux/ar71xx/files/arch/mips/ath79/mach-rbsxtlite.c
@@ -109,6 +109,7 @@ static struct gpio_led rbsxtlite_leds_gpio[] __initdata = {
 	{
 		.name		=	"rb:green:power",
 		.gpio		=	SXTLITE_GPIO_LED_POWER,
+		.default_state	=	LEDS_GPIO_DEFSTATE_KEEP,
 	},
 };
 

commit e6a3ad8e9ae2fd89a82e085e18076a8dd137c032

ar71xx: fix RouterBOARD Power LED default state
RouterBOARD(s) bootloader actully turns Power LED off just before
it starts the kernel. So we need to set the LED default status to On
instead of Keep in order to keep LED on during kernel boot.

This change fixes Power LED off during the kernel boot on the RB91x and
SXT Lite boards.

Fixes: 117b0f6cfb ("ar71xx: keep the RouterBOARD Power LED in On state")

CC: Mathias Kresin <dev@kresin.me>
Signed-off-by: Sergey Ryazanov <ryazanov.s.a@gmail.com>

target/linux/ar71xx/files/arch/mips/ath79/mach-rb91x.c
@@ -219,7 +219,7 @@ static struct gpio_led rb711gr100_leds[] __initdata = {
 		.name		= "rb:green:power",
 		.gpio		= RB91X_GPIO_LED_POWER,
 		.active_low	= 0,
-		.default_state	= LEDS_GPIO_DEFSTATE_KEEP,
+		.default_state	= LEDS_GPIO_DEFSTATE_ON,
 	},
 };
 

target/linux/ar71xx/files/arch/mips/ath79/mach-rbsxtlite.c
@@ -115,7 +115,7 @@ static struct gpio_led rbsxtlite_leds_gpio[] __initdata = {
 	{
 		.name		=	"rb:green:power",
 		.gpio		=	SXTLITE_GPIO_LED_POWER,
-		.default_state	=	LEDS_GPIO_DEFSTATE_KEEP,
+		.default_state	=	LEDS_GPIO_DEFSTATE_ON,
 	},
 };
 