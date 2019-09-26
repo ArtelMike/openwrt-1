ArtelMike/openwrt-1

commit ba6d1085b5430a5d2cb4bf4fdf27cf2222332d9c

treewide: use the generic board_name function
Use the generic function instead ot the target specific ones.

Signed-off-by: Mathias Kresin <dev@kresin.me>

target/linux/brcm47xx/base-files/lib/upgrade/platform.sh
@@ -6,12 +6,8 @@ get_magic_long_at() {
 	dd if="$1" skip=$2 bs=1 count=4 2>/dev/null | hexdump -v -n 4 -e '1/1 "%02x"'
 }
 
-platform_machine() {
-	grep "machine" /proc/cpuinfo | sed "s/.*:[ \t]*//"
-}
-
 platform_expected_image() {
-	local machine=$(platform_machine)
+	local machine=$(board_name)
 
 	case "$machine" in
 		"Netgear WGR614 V8")	echo "chk U12H072T00_NETGEAR"; return;;

target/linux/apm821xx/base-files/lib/upgrade/platform.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-. /lib/apm821xx.sh
-
 PART_NAME=firmware
-RAMFS_COPY_DATA=/lib/apm821xx.sh
 
 platform_check_image() {
-	local board=$(apm821xx_board_name)
+	local board=$(board_name)
 
 	[ "$#" -gt 1 ] && return 1
 
@@ -36,7 +33,7 @@ platform_check_image() {
 }
 
 platform_pre_upgrade() {
-	local board=$(apm821xx_board_name)
+	local board=$(board_name)
 
 	case "$board" in
 	mr24|\
@@ -54,7 +51,7 @@ platform_pre_upgrade() {
 }
 
 platform_do_upgrade() {
-	local board=$(apm821xx_board_name)
+	local board=$(board_name)
 
 	case "$board" in
 	mbl)
@@ -68,7 +65,7 @@ platform_do_upgrade() {
 }
 
 platform_copy_config() {
-	local board=$(apm821xx_board_name)
+	local board=$(board_name)
 
 	case "$board" in
 	mbl)

target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -184,7 +184,7 @@ alfa_check_image() {
 }
 
 platform_nand_board_name() {
-	local board=$(ar71xx_board_name)
+	local board=$(board_name)
 
 	case "$board" in
 	rb*) echo "routerboard";;
@@ -193,7 +193,7 @@ platform_nand_board_name() {
 }
 
 platform_check_image() {
-	local board=$(ar71xx_board_name)
+	local board=$(board_name)
 	local magic="$(get_magic_word "$1")"
 	local magic_long="$(get_magic_long "$1")"
 
@@ -668,7 +668,7 @@ platform_check_image() {
 }
 
 platform_pre_upgrade() {
-	local board=$(ar71xx_board_name)
+	local board=$(board_name)
 
 	case "$board" in
 	c-60|\
@@ -727,7 +727,7 @@ platform_pre_upgrade() {
 }
 
 platform_nand_pre_upgrade() {
-	local board=$(ar71xx_board_name)
+	local board=$(board_name)
 
 	case "$board" in
 	rb*)
@@ -742,7 +742,7 @@ platform_nand_pre_upgrade() {
 }
 
 platform_do_upgrade() {
-	local board=$(ar71xx_board_name)
+	local board=$(board_name)
 
 	case "$board" in
 	all0258n)

target/linux/bcm53xx/base-files/lib/upgrade/platform.sh
@@ -8,10 +8,6 @@ get_magic_long_at() {
 	dd if="$1" skip=$2 bs=1 count=4 2>/dev/null | hexdump -v -e '1/1 "%02x"'
 }
 
-platform_machine() {
-	cat /proc/device-tree/compatible | tr '\0' '\t' | cut -f 1
-}
-
 platform_flash_type() {
 	# On NAND devices "rootfs" is UBI volume, so won't be find in /proc/mtd
 	grep -q "\"rootfs\"" /proc/mtd && {
@@ -23,7 +19,7 @@ platform_flash_type() {
 }
 
 platform_expected_image() {
-	local machine=$(platform_machine)
+	local machine=$(board_name)
 
 	case "$machine" in
 		"dlink,dir-885l")	echo "seama wrgac42_dlink.2015_dir885l"; return;;

target/linux/imx6/base-files/lib/upgrade/platform.sh
@@ -2,10 +2,8 @@
 # Copyright (C) 2010-2015 OpenWrt.org
 #
 
-. /lib/imx6.sh
-
 platform_check_image() {
-	local board=$(imx6_board_name)
+	local board=$(board_name)
 
 	case "$board" in
 	*gw5*)
@@ -19,7 +17,7 @@ platform_check_image() {
 }
 
 platform_pre_upgrade() {
-	local board=$(imx6_board_name)
+	local board=$(board_name)
 
 	case "$board" in
 	*gw5*)

target/linux/ipq806x/base-files/lib/upgrade/platform.sh
@@ -1,5 +1,3 @@
-. /lib/ipq806x.sh
-
 PART_NAME=firmware
 REQUIRE_IMAGE_METADATA=1
 
@@ -11,9 +9,7 @@ platform_check_image() {
 }
 
 platform_do_upgrade() {
-	local board=$(ipq806x_board_name)
-
-	case "$board" in
+	case "$(board_name)" in
 	ap148 |\
 	ap-dk04.1-c1 |\
 	d7800 |\
@@ -43,9 +39,7 @@ platform_do_upgrade() {
 }
 
 platform_nand_pre_upgrade() {
-	local board=$(ipq806x_board_name)
-
-	case "$board" in
+	case "$(board_name)" in
 	nbg6817)
 		zyxel_do_upgrade "$1"
 		;;

target/linux/ixp4xx/base-files/lib/upgrade/platform.sh
@@ -1,7 +1,3 @@
-. /lib/ixp4xx.sh
-
-RAMFS_COPY_DATA="/lib/ixp4xx.sh"
-
 CI_BLKSZ=65536
 CI_LDADR=0x00800000
 
@@ -86,7 +82,7 @@ platform_do_upgrade_combined() {
 }
 
 platform_check_image() {
-	local board=$(ixp4xx_board_name)
+	local board=$(board_name)
 	local magic="$(get_magic_word "$1")"
 	local partitions=$(platform_find_partitions)
 	local kernelpart=$(platform_find_kernelpart "${partitions#*:}")
@@ -127,7 +123,7 @@ platform_check_image() {
 }
 
 platform_do_upgrade() {
-	local board=$(ixp4xx_board_name)
+	local board=$(board_name)
 
 	v "board=$board"
 	case "$board" in

target/linux/kirkwood/base-files/lib/upgrade/platform.sh
@@ -1,11 +1,9 @@
-. /lib/kirkwood.sh
-
 RAMFS_COPY_BIN='fw_printenv fw_setenv'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
 platform_check_image() {
 	[ "$#" -gt 1 ] && return 1
-	local board="$(kirkwood_board_name)"
+	local board="$(board_name)"
 	local magic="$(get_magic_long "$1")"
 
 	case "$board" in
@@ -28,7 +26,7 @@ platform_check_image() {
 }
 
 platform_do_upgrade() {
-	local board="$(kirkwood_board_name)"
+	local board="$(board_name)"
 
 	case "$board" in
 	"linksys-audi"|\

commit e6bbe5fbd9994a65304cd5c0a54d9498b463c756

brcm47xx: sysupgrade: fix device model detection
$(board_name) was providing content on "boardtype" (and optionally
"boardnum") NVRAM values. That function requires & expects more specific
and detailed model name extracted from the /proc/cpuinfo.

Fixes: ba6d1085b543 ("treewide: use the generic board_name function")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

target/linux/brcm47xx/base-files/lib/upgrade/platform.sh
@@ -15,9 +15,9 @@ get_le_long_at() {
 }
 
 platform_expected_image() {
-	local machine=$(board_name)
+	local model="$(cat /tmp/sysinfo/model)"
 
-	case "$machine" in
+	case "$model" in
 		"Netgear WGR614 V8")	echo "chk U12H072T00_NETGEAR"; return;;
 		"Netgear WGR614 V9")	echo "chk U12H094T00_NETGEAR"; return;;
 		"Netgear WGR614 V10")	echo "chk U12H139T01_NETGEAR"; return;;