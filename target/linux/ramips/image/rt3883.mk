ArtelMike/openwrt-1

commit a74ce2512bd2f38a222115671333ec1c04b9013d

ramips: add usb-ledtrig-usbport to DEVICE_PACKAGES of CY-SWR1100
CY-SWR1100 has a USB LED but kmod-usb-ledtrig-usbport is missing
in default images. This commit adds it.

Signed-off-by: Sungbo Eo <mans0n@gorani.run>
[changed commit title, backported to 19.07]
Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
(cherry picked from commit 5e0fcf837f78643a7be5d579947f922b5d5060cf)

target/linux/ramips/image/rt3883.mk
@@ -24,7 +24,8 @@ define Device/cy-swr1100
   KERNEL := $(KERNEL_DTB)
   SEAMA_SIGNATURE := wrgnd10_samsung_ss815
   DEVICE_TITLE := Samsung CY-SWR1100
-  DEVICE_PACKAGES := kmod-usb-core kmod-usb-ohci kmod-usb2 swconfig
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb-ohci kmod-usb2 | \
+	kmod-usb-ledtrig-usbport swconfig
 endef
 TARGET_DEVICES += cy-swr1100
 

commit a6e731508364ecbf13d9d91ff97237f6d61b813c

ramips: rt3833: fix build breakage
Commit a74ce2512bd2 ("ramips: add usb-ledtrig-usbport to DEVICE_PACKAGES
of CY-SWR1100") added stray | during backport which caused build
breakage on the buildbots:

 bash: -c: line 0: syntax error near unexpected token `|'
 bash: -c: line 0: `echo kmod-usb-core kmod-usb-ledtrig-usbport kmod-usb-ohci kmod-usb2 swconfig | | mkhash md5 | head -c 8'

Fixes: a74ce2512bd2 ("ramips: add usb-ledtrig-usbport to DEVICE_PACKAGES of CY-SWR1100")
Signed-off-by: Petr Štetiar <ynezz@true.cz>

target/linux/ramips/image/rt3883.mk
@@ -24,7 +24,7 @@ define Device/cy-swr1100
   KERNEL := $(KERNEL_DTB)
   SEAMA_SIGNATURE := wrgnd10_samsung_ss815
   DEVICE_TITLE := Samsung CY-SWR1100
-  DEVICE_PACKAGES := kmod-usb-core kmod-usb-ohci kmod-usb2 | \
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb-ohci kmod-usb2 \
 	kmod-usb-ledtrig-usbport swconfig
 endef
 TARGET_DEVICES += cy-swr1100