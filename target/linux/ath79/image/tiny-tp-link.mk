openwrt/openwrt

ath79: add support for TP-Link TL-WA850RE v1
This ports support for the TL-WA850RE v1 range extender from ar71xx
to ath79.

Specifications:
  Board: AP123 / AR9341 rev. 3
  Flash/RAM: 4/32 MiB
  CPU: 535 MHz
  WiFi: 2.4 GHz b/g/n
  Ethernet: 1 port (100M)

Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/ath79/image/tiny-tp-link.mk
@@ -66,6 +66,17 @@ define Device/tplink_tl-mr3420-v2
 endef
 TARGET_DEVICES += tplink_tl-mr3420-v2
 
+define Device/tplink_tl-wa850re-v1
+  $(Device/tplink-4mlzma)
+  SOC := ar9341
+  DEVICE_MODEL := TL-WA850RE
+  DEVICE_VARIANT := v1
+  TPLINK_HWID := 0x08500001
+  DEVICE_PACKAGES := rssileds
+  SUPPORTED_DEVICES += tl-wa850re
+endef
+TARGET_DEVICES += tplink_tl-wa850re-v1
+
 define Device/tplink_tl-wa901nd-v2
   $(Device/tplink-4m)
   SOC := ar9132

ArtelMike/openwrt-1

Flashing instructions:
Upload the factory image via the vendor firmware upgrade option.
Recovery:
Note that this device does not provide TFTP via ethernet like many
other TP-Link devices do. You will have to open the case if you
require recovery beyond failsafe.
(cherry picked from commit 9712a5e62687643e0b0f669bc72e0062def0f5b3)

target/linux/ath79/image/tiny-tp-link.mk
@@ -51,6 +51,16 @@ define Device/tplink_tl-mr3420-v1
 endef
 TARGET_DEVICES += tplink_tl-mr3420-v1
 
+define Device/tplink_tl-wa850re-v1
+  $(Device/tplink-4mlzma)
+  ATH_SOC := ar9341
+  DEVICE_TITLE := TP-Link TL-WA850RE v1
+  TPLINK_HWID := 0x08500001
+  DEVICE_PACKAGES := rssileds
+  SUPPORTED_DEVICES += tl-wa850re
+endef
+TARGET_DEVICES += tplink_tl-wa850re-v1
+
 define Device/tplink_tl-wa901nd-v2
   $(Device/tplink-4m)
   ATH_SOC := ar9132