openwrt/openwrt

ath79: add support for TP-Link TL-WA860RE v1
This ports support for the TL-WA860RE v1 range extender from ar71xx
to ath79.

Specifications:
  Board: AP123 / AR9341 rev. 3
  Flash/RAM: 4/32 MiB
  CPU: 535 MHz
  WiFi: 2.4 GHz b/g/n
  Ethernet: 1 port (100M)
  Two external antennas

Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/ath79/image/tiny-tp-link.mk
@@ -77,6 +77,16 @@ define Device/tplink_tl-wa850re-v1
 endef
 TARGET_DEVICES += tplink_tl-wa850re-v1
 
+define Device/tplink_tl-wa860re-v1
+  $(Device/tplink-4mlzma)
+  SOC := ar9341
+  DEVICE_MODEL := TL-WA860RE
+  DEVICE_VARIANT := v1
+  TPLINK_HWID := 0x08600001
+  SUPPORTED_DEVICES += tl-wa860re
+endef
+TARGET_DEVICES += tplink_tl-wa860re-v1
+
 define Device/tplink_tl-wa901nd-v2
   $(Device/tplink-4m)
   SOC := ar9132

ArtelMike/openwrt-1

Flashing instructions:
  Upload the factory image via the vendor firmware upgrade option.
Recovery:
  Note that this device does not provide TFTP via ethernet like many
  other TP-Link devices do. You will have to open the case if you
  require recovery beyond failsafe.
Tested-by: Sebastian Knapp <sebastian4842@outlook.com>
(cherry picked from commit 63579a355ab2052d8d666c0de6ac11235117ec5e)

target/linux/ath79/image/tiny-tp-link.mk
@@ -61,6 +61,15 @@ define Device/tplink_tl-wa850re-v1
 endef
 TARGET_DEVICES += tplink_tl-wa850re-v1
 
+define Device/tplink_tl-wa860re-v1
+  $(Device/tplink-4mlzma)
+  ATH_SOC := ar9341
+  DEVICE_TITLE := TP-Link TL-WA860RE v1
+  TPLINK_HWID := 0x08600001
+  SUPPORTED_DEVICES += tl-wa860re
+endef
+TARGET_DEVICES += tplink_tl-wa860re-v1
+
 define Device/tplink_tl-wa901nd-v2
   $(Device/tplink-4m)
   ATH_SOC := ar9132