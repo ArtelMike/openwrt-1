openwrt/openwrt

ath79: add support for TP-Link TL-WDR4310 v1
This device seems to be identical to the TL-WDR4300, just with
different release date/region and TPLINK_HWID.

Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/ath79/image/generic-tp-link.mk
@@ -418,6 +418,17 @@ define Device/tplink_tl-wdr4300-v1-il
 endef
 TARGET_DEVICES += tplink_tl-wdr4300-v1-il
 
+define Device/tplink_tl-wdr4310-v1
+  $(Device/tplink-8mlzma)
+  SOC := ar9344
+  DEVICE_MODEL := TL-WDR4310
+  DEVICE_VARIANT := v1
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport
+  TPLINK_HWID := 0x43100001
+  SUPPORTED_DEVICES += tl-wdr4300
+endef
+TARGET_DEVICES += tplink_tl-wdr4310-v1
+
 define Device/tplink_tl-wdr4900-v2
   $(Device/tplink-8mlzma)
   SOC := qca9558

ArtelMike/openwrt-1

Support is added based on the ar71xx implementation.
(cherry picked from commit b99672d35fac1515b79c7e37c64c20e0d5c698dc)

target/linux/ath79/image/generic-tp-link.mk
@@ -273,6 +273,16 @@ define Device/tplink_tl-wdr4300-v1-il
 endef
 TARGET_DEVICES += tplink_tl-wdr4300-v1-il
 
+define Device/tplink_tl-wdr4310-v1
+  $(Device/tplink-8mlzma)
+  ATH_SOC := ar9344
+  DEVICE_TITLE := TP-Link TL-WDR4310 v1
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport
+  TPLINK_HWID := 0x43100001
+  SUPPORTED_DEVICES += tl-wdr4300
+endef
+TARGET_DEVICES += tplink_tl-wdr4310-v1
+
 define Device/tplink_tl-wdr4900-v2
   $(Device/tplink-8mlzma)
   ATH_SOC := qca9558