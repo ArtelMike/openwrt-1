ArtelMike/openwrt-1

commit 38bdea474cc4c8504c2e3a4048b92326837f5b70

kernel: Update kernel 4.9 to version 4.9.229
Fixes:
- CVE-2020-10757

Run tested: ar71xx
Build tested: ar71xx

Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

target/linux/ar71xx/files/drivers/mtd/nand/rb4xx_nand.c
@@ -331,7 +331,7 @@ static int rb4xx_nand_probe(struct platform_device *pdev)
 	return 0;
 
 err_release_nand:
-	nand_release(mtd);
+	nand_release(&info->chip);
 err_set_drvdata:
 	platform_set_drvdata(pdev, NULL);
 err_free_info:
@@ -352,7 +352,7 @@ static int rb4xx_nand_remove(struct platform_device *pdev)
 {
 	struct rb4xx_nand_info *info = platform_get_drvdata(pdev);
 
-	nand_release(rbinfo_to_mtd(info));
+	nand_release(&info->chip));
 	platform_set_drvdata(pdev, NULL);
 	kfree(info);
 	gpio_free(RB4XX_NAND_GPIO_NCE);

include/kernel-version.mk
@@ -2,10 +2,10 @@
 
 LINUX_RELEASE?=1
 
-LINUX_VERSION-4.9 = .223
+LINUX_VERSION-4.9 = .229
 LINUX_VERSION-4.14 = .187
 
-LINUX_KERNEL_HASH-4.9.223 = cf5300e6f5d8c66c2bed8f00d53f9c58103731809862427012e4010f5d782ae5
+LINUX_KERNEL_HASH-4.9.229 = 3256c2835fd95a1a739603e78b02d363eac2ce73a39fa19b13b32da4fc370fdc
 LINUX_KERNEL_HASH-4.14.187 = 5b223475eaeea196aa7e127d3f253bca5c35d8afdc72ca75230ce1ecdd1454bd
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))

target/linux/ar71xx/files/drivers/mtd/nand/ar934x_nfc.c
@@ -1562,7 +1562,7 @@ ar934x_nfc_remove(struct platform_device *pdev)
 	nfc = platform_get_drvdata(pdev);
 	if (nfc) {
 		mtd = ar934x_nfc_to_mtd(nfc);
-		nand_release(mtd);
+		nand_release(&nfc->nand_chip);
 		ar934x_nfc_free_buf(nfc);
 		free_irq(nfc->irq, nfc);
 	}

target/linux/ar71xx/files/drivers/mtd/nand/rb750_nand.c
@@ -389,7 +389,7 @@ static int rb750_nand_probe(struct platform_device *pdev)
 	return 0;
 
 err_release_nand:
-	nand_release(mtd);
+	nand_release(&info->chip);
 err_set_drvdata:
 	platform_set_drvdata(pdev, NULL);
 err_free_info:
@@ -401,7 +401,7 @@ static int rb750_nand_remove(struct platform_device *pdev)
 {
 	struct rb750_nand_info *info = platform_get_drvdata(pdev);
 
-	nand_release(rbinfo_to_mtd(info));
+	nand_release(&info->chip);
 	platform_set_drvdata(pdev, NULL);
 	kfree(info);
 

target/linux/ar71xx/files/drivers/mtd/nand/rb91x_nand.c
@@ -430,7 +430,7 @@ static int rb91x_nand_probe(struct platform_device *pdev)
 	return 0;
 
 err_release_nand:
-	nand_release(mtd);
+	nand_release(&rbni->chip);
 	return ret;
 }
 
@@ -438,7 +438,7 @@ static int rb91x_nand_remove(struct platform_device *pdev)
 {
 	struct rb91x_nand_info *info = platform_get_drvdata(pdev);
 
-	nand_release(rbinfo_to_mtd(info));
+	nand_release(&rbni->chip);
 
 	return 0;
 }

target/linux/ar71xx/patches-4.9/910-unaligned_access_hacks.patch
@@ -267,7 +267,7 @@
  		case IPV6_2292HOPOPTS:
 --- a/net/ipv6/ip6_gre.c
 +++ b/net/ipv6/ip6_gre.c
-@@ -397,7 +397,7 @@ static void ip6gre_err(struct sk_buff *s
+@@ -400,7 +400,7 @@ static void ip6gre_err(struct sk_buff *s
  		return;
  	ipv6h = (const struct ipv6hdr *)skb->data;
  	greh = (const struct gre_base_hdr *)(skb->data + offset);
@@ -888,7 +888,7 @@
  		*sum = csum_fold(csum_partial(diff, sizeof(diff),
 --- a/drivers/net/vxlan.c
 +++ b/drivers/net/vxlan.c
-@@ -1800,15 +1800,15 @@ static int vxlan_build_skb(struct sk_buf
+@@ -1804,15 +1804,15 @@ static int vxlan_build_skb(struct sk_buf
  		goto out_free;
  
  	vxh = (struct vxlanhdr *) __skb_push(skb, sizeof(*vxh));

target/linux/brcm2708/patches-4.9/950-0028-kbuild-Ignore-dtco-targets-when-filtering-symbols.patch
@@ -9,7 +9,7 @@ Subject: [PATCH] kbuild: Ignore dtco targets when filtering symbols
 
 --- a/scripts/Kbuild.include
 +++ b/scripts/Kbuild.include
-@@ -294,7 +294,7 @@ ksym_dep_filter =
+@@ -295,7 +295,7 @@ ksym_dep_filter =
  	    $(CPP) $(call flags_nodeps,c_flags) -D__KSYM_DEPS__ $< ;;        \
  	  as_*_S|cpp_s_S)                                                    \
  	    $(CPP) $(call flags_nodeps,a_flags) -D__KSYM_DEPS__ $< ;;        \

target/linux/brcm2708/patches-4.9/950-0044-Add-SMI-NAND-driver.patch
@@ -312,7 +312,7 @@ Signed-off-by: Luke Wren <wren6991@gmail.com>
 +	if (!ret)
 +		return 0;
 +
-+	nand_release(mtd);
++	nand_release(this);
 +	return -EINVAL;
 +}
 +
@@ -320,7 +320,7 @@ Signed-off-by: Luke Wren <wren6991@gmail.com>
 +{
 +	struct bcm2835_smi_nand_host *host = platform_get_drvdata(pdev);
 +
-+	nand_release(&host->mtd);
++	nand_release(&host->nand_chip);
 +
 +	return 0;
 +}

commit 44306b15f66b0f38113ee1611dd134a34c26815e

ar71xx: Fix mikrotik NAND compile problem
There is one closing bracket too much.

Fixes: 38bdea474cc4 ("kernel: Update kernel 4.9 to version 4.9.229")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
(cherry picked from commit 4507fdee2e1aacf8f99ea79ffa4727fb2fdcae51)

target/linux/ar71xx/files/drivers/mtd/nand/rb4xx_nand.c
@@ -352,7 +352,7 @@ static int rb4xx_nand_remove(struct platform_device *pdev)
 {
 	struct rb4xx_nand_info *info = platform_get_drvdata(pdev);
 
-	nand_release(&info->chip));
+	nand_release(&info->chip);
 	platform_set_drvdata(pdev, NULL);
 	kfree(info);
 	gpio_free(RB4XX_NAND_GPIO_NCE);