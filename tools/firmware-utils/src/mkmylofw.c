ArtelMike/openwrt-1

commit eb48c36fd2d0b91854504226a40d435e2f5d8e1b

firmware-utils: fix few random warnings
1. -Wunused-label
2. -Wparentheses
3. -Wmisleading-indentation
4. -Wformat-overflow

Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

tools/firmware-utils/src/mkmylofw.c
@@ -98,7 +98,7 @@ struct cpx_board {
 #define CPX_BOARD_AR23XX(_did, _flash, _mod, _name, _desc) \
 	CPX_BOARD(_did, _flash, _mod, _name, _desc, 0x10000, 0x10000)
 
-#define ALIGN(x,y)	((x)+((y)-1)) & ~((y)-1)
+#define ALIGN(x,y)	(((x)+((y)-1)) & ~((y)-1))
 
 char	*progname;
 char	*ofname = NULL;

tools/firmware-utils/src/dgn3500sum.c
@@ -104,7 +104,7 @@ int main(int argc, char** argv)
   char *endptr, *buffer, *p;
   int count;  // size of file in bytes
   unsigned short sum = 0, sum1 = 0;
-  char sumbuf[9];
+  char sumbuf[8 + 8 + 1];
 
   if(argc < 3) {
     printf("ERROR: Argument missing!\n\nUsage %s filename starting offset in hex [PID code]\n\n", argv[0]);

tools/firmware-utils/src/mkdniimg.c
@@ -187,7 +187,6 @@ int main(int argc, char *argv[])
 
 	res = EXIT_SUCCESS;
 
- out_flush:
 	fflush(outfile);
 
  err_close_out:

tools/firmware-utils/src/mkplanexfw.c
@@ -247,7 +247,6 @@ int main(int argc, char *argv[])
 
 	res = EXIT_SUCCESS;
 
- out_flush:
 	fflush(outfile);
 
  err_close_out:

tools/firmware-utils/src/mktitanimg.c
@@ -284,7 +284,7 @@ int main(int argc, char* argv[], char* env[])
 			else
 				padding = align - ((section->raw_size+ section->offset) % align);
 
-				section->total_size=section->raw_size + padding;
+			section->total_size=section->raw_size + padding;
 		}
 		else{
 			#define EXTRA_BLOCK 0x10000

tools/firmware-utils/src/osbridge-crc.c
@@ -163,7 +163,6 @@ int main(int argc, char *argv[])
 
 	res = EXIT_SUCCESS;
 
- out_flush:
 	fflush(outfile);
 
  err_close_out:

tools/firmware-utils/src/pc1crypt.c
@@ -339,7 +339,6 @@ int main(int argc, char *argv[])
 
 	res = EXIT_SUCCESS;
 
- out_flush:
 	fflush(outfile);
 
  err_close_out:

commit 5eac87c895ef5a8c434dc466f31e5eb134139b9d

firmware-utils: mkmylofw: fix blocks padding
The old code didn't make sense as it was using "len" variable which was
guaranteed to be always 0. Loop right above broken code is:
while (len > 0) { }

With this recent ALIGN macro fix this resulted in subtracting block size
from 0 and calling write_out_padding() with a negative length.

To calculate amount of bytes needed for padding & alignment it should be
enough to use % 4.

Fixes: eb48c36fd2d0 ("firmware-utils: fix few random warnings")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

tools/firmware-utils/src/mkmylofw.c
@@ -583,7 +583,7 @@ write_out_file(FILE *outfile, struct fw_block *block, uint32_t *crc)
 	fclose(f);
 
 	/* align next block on a 4 byte boundary */
-	len = ALIGN(len,4) - block->size;
+	len = block->size % 4;
 	if (write_out_padding(outfile, len, 0xFF, crc))
 		return -1;
 