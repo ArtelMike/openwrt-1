750a986e8a44e554fe63f5cde5aaa0c48c25bf52
Michael McCarthy <mmccarthy@artel.com>
2023-01-18 10:38:59 -0500

    Remove private and proprietary stuff from public repo

--- files/usr/artel/bin/doit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-AVSPATH=/usr/artel/bin
-
-$AVSPATH/prog5338
-bash $AVSPATH/loadfpga.sh avsOGLoadLink
-bash $AVSPATH/loadaxi.sh
-exit 0
-
--- files/usr/artel/bin/startcan.sh
+++
@@ -1,5 +0,0 @@
-#
-ip link set can2 type can bitrate 1000000 triple-sampling on
-ifconfig can2 up
-exit 0
-
